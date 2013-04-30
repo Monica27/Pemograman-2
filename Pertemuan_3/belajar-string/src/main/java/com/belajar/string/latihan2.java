6dda0e3d99e027488da75e6bf77c0b98b8c467dc
unknown <novi adi@.(none)>
2013-04-30 11:58:57 +0700

    pemrograman2

---
+++ Pertemuan_3/belajar-string/src/main/java/com/belajar/string/latihan2.java
@@ -0,0 +1,10 @@
+package com.belajar.string;
+
+public class latihan2{
+	public static void main(String[] args){
+		String s1="Go to";
+		String s2="Univeritas Pancasila";
+		
+		System.out.println(s1.concat(s2));
+		}
+		}
---
+++ Pertemuan_3/belajar-string/src/main/java/com/belajar/string/latihan6.java
@@ -0,0 +1,8 @@
+package com.belajar.string;
+public class latihan6{
+	public static void main(String[] args){
+		String text = "Hello World";
+		System.out.println(text.toUpperCase());
+		}
+		}
+		
